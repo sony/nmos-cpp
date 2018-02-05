9bca76ab957796beabebe2448fd3a2fff34e412d
garethsb-sony <gareth.sylvester-bradley@sony.com>
2018-02-05 13:15:08 +0000

    Use shared_mutex to provide shared and exclusive locking semantics for nmos::read_lock and nmos::write_lock respectively
    (cherry picked from commit d36474cb95ff9fb27109a34a875b92d6046e2a3b, b76de967da730e1701260d5c3aa745f5b8883a4a)

---
+++ Development/bst/shared_mutex.h
@@ -0,0 +1,27 @@
+#ifndef BST_SHARED_MUTEX_H
+#define BST_SHARED_MUTEX_H
+
+// Provide bst::shared_mutex, bst::shared_lock etc. using either std:: or boost:: symbols
+
+// Note: If this isn't defined under the same condition as BST_THREAD_BOOST, adding shared_timed_mutex is problematic
+// because its timeout functionality won't be consistent with bst::chrono
+#ifndef BST_SHARED_MUTEX_BOOST
+
+#include <shared_mutex>
+namespace bst_shared_mutex = std;
+
+#else
+
+#include <boost/thread/lock_types.hpp>
+#include <boost/thread/shared_mutex.hpp>
+namespace bst_shared_mutex = boost;
+
+#endif
+
+namespace bst
+{
+    using bst_shared_mutex::shared_mutex;
+    using bst_shared_mutex::shared_lock;
+}
+
+#endif
--- Development/nmos/mutex.h
+++ Development/nmos/mutex.h
@@ -2,15 +2,16 @@
 #define NMOS_MUTEX_H
 
 #include <condition_variable>
+#include "bst/shared_mutex.h"
 
 namespace nmos
 {
-    typedef std::mutex mutex;
+    typedef bst::shared_mutex mutex;
 
-    typedef std::unique_lock<mutex> read_lock;
+    typedef bst::shared_lock<mutex> read_lock;
     typedef std::unique_lock<mutex> write_lock;
 
-    typedef std::condition_variable condition_variable;
+    typedef std::condition_variable_any condition_variable;
 }
 
 #endif
