53851d09f315c00d3142913ccc30951f6f2dea4e
garethsb-sony <gareth.sylvester-bradley@sony.com>
2018-02-26 13:50:39 +0000

    Add "host_target" to the mdns_api response, and make the long-winded route handlers for this API non-blocking like the NMOS APIs
    (cherry picked from commit b833d082023de25c5999e2968c46cbdc7b51affa)

--- Development/nmos/mdns_api.cpp
+++ Development/nmos/mdns_api.cpp
@@ -15,6 +15,7 @@ namespace nmos
             value result;
 
             result[U("name")] = value::string(utility::s2us(name));
+            result[U("host_target")] = value::string(utility::s2us(resolved.host_name));
             result[U("address")] = !resolved.ip_address.empty() ? value::string(utility::s2us(resolved.ip_address)) : value::null();
             result[U("port")] = resolved.port;
 
@@ -56,70 +57,76 @@ namespace nmos
 
             mdns_api.support(U("/x-mdns/") + nmos::experimental::patterns::mdnsServiceType.pattern + U("/?"), methods::GET, [&gate](http_request, http_response res, const string_t&, const route_parameters& parameters)
             {
-                const std::string serviceType = utility::us2s(parameters.at(nmos::experimental::patterns::mdnsServiceType.name));
-
-                std::unique_ptr<::mdns::service_discovery> discovery = ::mdns::make_discovery(gate);
-                std::vector<::mdns::service_discovery::browse_result> found;
-
-                if (discovery->browse(found, serviceType))
+                return pplx::create_task([]{}).then([&, res, parameters]() mutable
                 {
-                    std::map<std::string, value> results;
+                    const std::string serviceType = utility::us2s(parameters.at(nmos::experimental::patterns::mdnsServiceType.name));
 
-                    for (const auto& f : found)
+                    std::unique_ptr<::mdns::service_discovery> discovery = ::mdns::make_discovery(gate);
+                    std::vector<::mdns::service_discovery::browse_result> found;
+
+                    if (discovery->browse(found, serviceType))
                     {
-                        // check if we already have resolved this service
-                        if (results.end() != results.find(f.name)) continue;
+                        std::map<std::string, value> results;
 
-                        ::mdns::service_discovery::resolve_result resolved;
-
-                        if (discovery->resolve(resolved, f.name, f.type, f.domain, f.interface_id))
+                        for (const auto& f : found)
                         {
-                            results[f.name] = make_mdns_result(f.name, resolved);
-                        }
-                    }
+                            // check if we already have resolved this service
+                            if (results.end() != results.find(f.name)) continue;
 
-                    if (!results.empty())
-                    {
-                        set_reply(res, status_codes::OK,
-                            web::json::serialize(results, [](const std::map<std::string, value>::value_type& kv) { return kv.second; }),
-                            U("application/json"));
-                        res.headers().add(U("X-Total-Count"), results.size());
+                            ::mdns::service_discovery::resolve_result resolved;
+
+                            if (discovery->resolve(resolved, f.name, f.type, f.domain, f.interface_id))
+                            {
+                                results[f.name] = make_mdns_result(f.name, resolved);
+                            }
+                        }
+
+                        if (!results.empty())
+                        {
+                            set_reply(res, status_codes::OK,
+                                web::json::serialize(results, [](const std::map<std::string, value>::value_type& kv) { return kv.second; }),
+                                U("application/json"));
+                            res.headers().add(U("X-Total-Count"), results.size());
+                        }
+                        else
+                        {
+                            set_reply(res, status_codes::NotFound);
+                        }
                     }
                     else
                     {
                         set_reply(res, status_codes::NotFound);
                     }
-                }
-                else
-                {
-                    set_reply(res, status_codes::NotFound);
-                }
 
-                return pplx::task_from_result(true);
+                    return true;
+                });
             });
 
             mdns_api.support(U("/x-mdns/") + nmos::experimental::patterns::mdnsServiceType.pattern + U("/") + nmos::experimental::patterns::mdnsServiceName.pattern + U("/?"), methods::GET, [&gate](http_request, http_response res, const string_t&, const route_parameters& parameters)
             {
-                const std::string serviceType = utility::us2s(parameters.at(nmos::experimental::patterns::mdnsServiceType.name));
-                const std::string serviceName = utility::us2s(parameters.at(nmos::experimental::patterns::mdnsServiceName.name));
-
-                std::unique_ptr<::mdns::service_discovery> discovery = ::mdns::make_discovery(gate);
-                ::mdns::service_discovery::resolve_result resolved;
-
-                // When browsing, we use the default domain and then use the browse results' domain (and interface)
-                // but here, I'm not sure what we should specify?
-                if (discovery->resolve(resolved, serviceName, serviceType, "local."))
+                return pplx::create_task([]{}).then([&, res, parameters]() mutable
                 {
-                    value result = make_mdns_result(serviceName, resolved);
+                    const std::string serviceType = utility::us2s(parameters.at(nmos::experimental::patterns::mdnsServiceType.name));
+                    const std::string serviceName = utility::us2s(parameters.at(nmos::experimental::patterns::mdnsServiceName.name));
 
-                    set_reply(res, status_codes::OK, result);
-                }
-                else
-                {
-                    set_reply(res, status_codes::NotFound);
-                }
+                    std::unique_ptr<::mdns::service_discovery> discovery = ::mdns::make_discovery(gate);
+                    ::mdns::service_discovery::resolve_result resolved;
 
-                return pplx::task_from_result(true);
+                    // When browsing, we use the default domain and then use the browse results' domain (and interface)
+                    // but here, I'm not sure what we should specify?
+                    if (discovery->resolve(resolved, serviceName, serviceType, "local."))
+                    {
+                        value result = make_mdns_result(serviceName, resolved);
+
+                        set_reply(res, status_codes::OK, result);
+                    }
+                    else
+                    {
+                        set_reply(res, status_codes::NotFound);
+                    }
+
+                    return true;
+                });
             });
 
             nmos::add_api_finally_handler(mdns_api, gate);
