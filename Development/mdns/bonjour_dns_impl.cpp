5eae4f95a4dcdbca5344abe3450aa854aff0adca
garethsb-sony <gareth.sylvester-bradley@sony.com>
2018-03-07 13:09:14 +0000

    Use DNSServiceGetAddrInfo to ensure we get the address for both unicast and multicast registered names. Plus other tidying up.
    (cherry picked from commit 4e4a26504272a7b0a5afc3afd1ec01881a898f95)

--- Development/mdns/bonjour_dns_impl.cpp
+++ Development/mdns/bonjour_dns_impl.cpp
@@ -1,7 +1,9 @@
 #include "mdns/bonjour_dns_impl.h"
 
+#include <boost/asio/ip/address.hpp>
 #include <boost/algorithm/string/predicate.hpp>
 #include <boost/algorithm/string/erase.hpp>
+#include "dns_sd.h"
 #include "cpprest/basic_utils.h"
 #include "cpprest/host_utils.h"
 #include "slog/all_in_one.h"
@@ -158,6 +160,7 @@ namespace mdns
         {
             result = true;
 
+            // if name were empty, the callback would indicate what name was automatically chosen (and likewise for domain)
             m_services.push_back({ name, type, domain, sdRef });
 
             slog::log<slog::severities::info>(m_gate, SLOG_FLF) << "Registered advertisement for: " << name << "." << type << (domain.empty() ? "" : "." + domain);
@@ -181,7 +184,7 @@ namespace mdns
             {
                 const std::vector<unsigned char> txt_records = make_txt_records(records);
 
-                DNSServiceErrorType errorCode = DNSServiceUpdateRecord(service.sdRef, NULL, 0, (std::uint16_t)txt_records.size(), &txt_records[0], 0);
+                DNSServiceErrorType errorCode = DNSServiceUpdateRecord((DNSServiceRef)service.sdRef, NULL, 0, (std::uint16_t)txt_records.size(), &txt_records[0], 0);
                 
                 if (errorCode == kDNSServiceErr_NoError)
                 {
@@ -215,6 +218,7 @@ namespace mdns
             {
                 mdns::service_discovery::browse_result result{ serviceName, regtype, replyDomain, interfaceIndex };
 
+                slog::log<slog::severities::more_info>(impl->m_gate, SLOG_FLF) << "After DNSServiceBrowse, DNSServiceBrowseReply got service: " << result.name << " for regtype: " << result.type << " domain: " << result.domain;
                 found.push_back(result);
             }
 
@@ -248,7 +252,7 @@ namespace mdns
                 // wait for up to timeout seconds for a response
                 int wait_millis = (std::max)(0, (int)std::chrono::duration_cast<std::chrono::milliseconds>(absolute_timeout - std::chrono::system_clock::now()).count());
 
-                // get the next browse response
+                // process the next browse response
                 errorCode = DNSServiceProcessResult(client, wait_millis);
 
                 if (errorCode == kDNSServiceErr_NoError)
@@ -326,12 +330,12 @@ namespace mdns
         if (errorCode == kDNSServiceErr_NoError)
         {
             resolved.host_name = hosttarget;
-            slog::log<slog::severities::more_info>(impl->m_gate, SLOG_FLF) << "After DNSServiceResolve, get address for host: " << resolved.host_name;
-            // if we used DNSServiceGetAddrInfo here, it would handle the suffix itself?
+            // now that DNSServiceGetAddrInfo is being called afterwards, this result will generally be overwritten
             const auto ip_addresses = web::http::experimental::host_addresses(utility::s2us(without_suffix(resolved.host_name)));
             if (!ip_addresses.empty())
             {
                 resolved.ip_address = utility::us2s(ip_addresses[0]);
+                slog::log<slog::severities::more_info>(impl->m_gate, SLOG_FLF) << "After DNSServiceResolve, DNSServiceResolveReply got address: " << resolved.ip_address << " for host: " << resolved.host_name;
             }
             resolved.port = ntohs(port);
             resolved.txt_records = parse_txt_records(txtRecord, txtLen);
@@ -339,6 +343,62 @@ namespace mdns
         else
         {
             slog::log<slog::severities::error>(impl->m_gate, SLOG_FLF) << "After DNSServiceResolve, DNSServiceResolveReply received error: " << errorCode;
+        }
+    }
+
+    // this just shouldn't be this hard!
+    static boost::asio::ip::address from_sockaddr(const sockaddr& address)
+    {
+        if (address.sa_family == AF_INET)
+        {
+            auto addr_in = (const sockaddr_in&)address;
+            auto addr = addr_in.sin_addr.s_addr;
+
+            return boost::asio::ip::address_v4(ntohl(addr));
+        }
+        else if (address.sa_family == AF_INET6)
+        {
+            auto addr_in6 = (const sockaddr_in6&)address;
+            auto addr6 = addr_in6.sin6_addr.s6_addr;
+            boost::asio::ip::address_v6::bytes_type addr6_bytes;
+            std::copy_n(addr6, addr6_bytes.size(), addr6_bytes.begin());
+
+            //auto scope_id = addr_in6.sin6_scope_id;
+            //return boost::asio::ip::address_v6(addr6_bytes, scope_id);
+            return boost::asio::ip::address_v6(addr6_bytes);
+        }
+        else
+        {
+            return{};
+        }
+    }
+
+    static void DNSSD_API getaddrinfo_reply(
+        DNSServiceRef          sdRef,
+        const DNSServiceFlags  flags,
+        uint32_t               interfaceIndex,
+        DNSServiceErrorType    errorCode,
+        const char*            hostname,
+        const struct sockaddr* address,
+        uint32_t               ttl,
+        void*                  context)
+    {
+        bonjour_dns_impl* impl = (bonjour_dns_impl*)context;
+
+        service_discovery::resolve_result& resolved = *impl->m_resolved;
+
+        if (errorCode == kDNSServiceErr_NoError && 0 != address)
+        {
+            const auto ip_address = from_sockaddr(*address);
+            if (!ip_address.is_unspecified())
+            {
+                resolved.ip_address = ip_address.to_string();
+                slog::log<slog::severities::more_info>(impl->m_gate, SLOG_FLF) << "After DNSServiceGetAddrInfo, DNSServiceGetAddrInfoReply got address: " << resolved.ip_address << " for host: " << resolved.host_name;
+            }
+        }
+        else
+        {
+            slog::log<slog::severities::error>(impl->m_gate, SLOG_FLF) << "After DNSServiceGetAddrInfo, DNSServiceGetAddrInfoReply received error: " << errorCode;
         }
     }
 
@@ -352,12 +412,17 @@ namespace mdns
 
         slog::log<slog::severities::more_info>(m_gate, SLOG_FLF) << "DNSServiceResolve for name: " << name << " regtype: " << type << " domain: " << domain;
 
-        DNSServiceErrorType errorCode = DNSServiceResolve(&client, 0, interface_id, name.c_str(), type.c_str(), domain.c_str(), resolve_reply, this);
+        const auto absolute_timeout = std::chrono::system_clock::now() + std::chrono::seconds(timeout_secs);
+
+        DNSServiceErrorType errorCode = DNSServiceResolve(&client, 0, interface_id, name.c_str(), type.c_str(), domain.c_str(), (DNSServiceResolveReply)resolve_reply, this);
 
         if (errorCode == kDNSServiceErr_NoError)
         {
-            // wait for the resolve response
-            errorCode = DNSServiceProcessResult(client, (int)timeout_secs * 1000);
+            // wait for up to timeout seconds for a response
+            int wait_millis = (std::max)(0, (int)std::chrono::duration_cast<std::chrono::milliseconds>(absolute_timeout - std::chrono::system_clock::now()).count());
+
+            // process the resolve response
+            errorCode = DNSServiceProcessResult(client, wait_millis);
 
             if (errorCode == kDNSServiceErr_NoError)
             {
@@ -373,6 +438,37 @@ namespace mdns
             }
 
             DNSServiceRefDeallocate(client);
+
+            // for now, limited to IPv4
+            errorCode = DNSServiceGetAddrInfo(&client, 0, interface_id, kDNSServiceProtocol_IPv4, resolved.host_name.c_str(), getaddrinfo_reply, this);
+
+            if (errorCode == kDNSServiceErr_NoError)
+            {
+                // wait for up to timeout seconds for a response
+                int wait_millis = (std::max)(0, (int)std::chrono::duration_cast<std::chrono::milliseconds>(absolute_timeout - std::chrono::system_clock::now()).count());
+
+                // process the address info response
+                errorCode = DNSServiceProcessResult(client, wait_millis);
+
+                if (errorCode == kDNSServiceErr_NoError)
+                {
+                    // callback called
+                }
+                else if (errorCode == kDNSServiceErr_Timeout_)
+                {
+                    // timeout expired
+                }
+                else
+                {
+                    slog::log<slog::severities::error>(m_gate, SLOG_FLF) << "After DNSServiceGetAddrInfo, DNSServiceProcessResult reported error: " << errorCode;
+                }
+
+                DNSServiceRefDeallocate(client);
+            }
+            else
+            {
+                slog::log<slog::severities::error>(m_gate, SLOG_FLF) << "DNSServiceGetAddrInfo reported error: " << errorCode;
+            }
         }
         else
         {
@@ -392,7 +488,7 @@ namespace mdns
         // De-register anything that is registered
         for (const auto& service : m_services)
         {
-            DNSServiceRefDeallocate(service.sdRef);
+            DNSServiceRefDeallocate((DNSServiceRef)service.sdRef);
             slog::log<slog::severities::too_much_info>(m_gate, SLOG_FLF) << "Advertisement stopped for: " << service.name;
         }
 
--- Development/mdns/bonjour_dns_impl.h
+++ Development/mdns/bonjour_dns_impl.h
@@ -4,11 +4,6 @@
 #include "mdns/service_advertiser.h"
 #include "mdns/service_discovery.h"
 
-extern "C"
-{
-#include "dns_sd.h"
-}
-
 namespace mdns
 {
     class bonjour_dns_impl : public mdns::service_advertiser, public mdns::service_discovery
@@ -28,22 +23,23 @@ namespace mdns
         virtual bool browse(std::vector<browse_result>& found, const std::string& type, const std::string& domain, unsigned int timeout_seconds);
         virtual bool resolve(resolve_result& resolved, const std::string& name, const std::string& type, const std::string& domain, std::uint32_t interface_id, unsigned int timeout_seconds);
 
-        struct DNSService
+        struct service
         {
-            DNSService(const std::string& name, const std::string& type, const std::string& domain, const DNSServiceRef& sdRef)
+            service(const std::string& name, const std::string& type, const std::string& domain, void* sdRef)
                 : name(name)
                 , type(type)
                 , domain(domain)
                 , sdRef(sdRef)
             {}
 
-            const std::string name;
-            const std::string type;
-            const std::string domain;
-            const DNSServiceRef sdRef;
+            std::string name;
+            std::string type;
+            std::string domain;
+
+            void* sdRef;
         };
 
-        std::vector<DNSService> m_services;
+        std::vector<service> m_services;
         slog::base_gate& m_gate;
 
         // browse/resolve in-flight state
