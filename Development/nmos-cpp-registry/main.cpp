9f4e7f0eeceb8a5fd442449321ad4127c3581052
garethsb-sony <gareth.sylvester-bradley@sony.com>
2018-11-08 11:33:02 +0000

    Allow nmos-cpp-registry to be configured to use the same port for all HTTP APIs (the Query WebSocket API must still be on a different port for now)
    (cherry picked from commit 654df89726b350839a25adbb7639d2190378022b)

--- Development/nmos-cpp-registry/main.cpp
+++ Development/nmos-cpp-registry/main.cpp
@@ -107,34 +107,25 @@ int main(int argc, char* argv[])
         slog::log<slog::severities::info>(gate, SLOG_FLF) << "Configuring nmos-cpp registry with its primary Registration API at: " << nmos::fields::host_address(registry_model.settings) << ":" << nmos::fields::registration_port(registry_model.settings);
         slog::log<slog::severities::info>(gate, SLOG_FLF) << "Configuring nmos-cpp registry with its primary Query API at: " << nmos::fields::host_address(registry_model.settings) << ":" << nmos::fields::query_port(registry_model.settings);
 
-        // Configure the mDNS Service Discovery API
+        // Set up the APIs, assigning them to the configured ports
 
-        web::http::experimental::listener::api_router mdns_api = nmos::experimental::make_mdns_api(registry_model, gate);
-        web::http::experimental::listener::http_listener mdns_listener(web::http::experimental::listener::make_listener_uri(nmos::experimental::fields::mdns_port(registry_model.settings)));
-        nmos::support_api(mdns_listener, mdns_api, gate);
+        std::map<int, web::http::experimental::listener::api_router> port_routers;
+
+        // Configure the DNS-SD Browsing API
+
+        port_routers[nmos::experimental::fields::mdns_port(registry_model.settings)].mount({}, nmos::experimental::make_mdns_api(registry_model, gate));
 
         // Configure the Settings API
 
-        web::http::experimental::listener::api_router settings_api = nmos::experimental::make_settings_api(registry_model, level, gate);
-        web::http::experimental::listener::http_listener settings_listener(web::http::experimental::listener::make_listener_uri(nmos::experimental::fields::settings_port(registry_model.settings)));
-        nmos::support_api(settings_listener, settings_api, gate);
+        port_routers[nmos::experimental::fields::settings_port(registry_model.settings)].mount({}, nmos::experimental::make_settings_api(registry_model, level, gate));
 
         // Configure the Logging API
 
-        web::http::experimental::listener::api_router logging_api = nmos::experimental::make_logging_api(log_model, gate);
-        web::http::experimental::listener::http_listener logging_listener(web::http::experimental::listener::make_listener_uri(nmos::experimental::fields::logging_port(registry_model.settings)));
-        nmos::support_api(logging_listener, logging_api, gate);
-
-        // Configure the NMOS APIs
-
-        web::http::experimental::listener::http_listener_config listener_config;
-        listener_config.set_backlog(nmos::fields::listen_backlog(registry_model.settings));
+        port_routers[nmos::experimental::fields::logging_port(registry_model.settings)].mount({}, nmos::experimental::make_logging_api(log_model, gate));
 
         // Configure the Query API
 
-        web::http::experimental::listener::api_router query_api = nmos::make_query_api(registry_model, gate);
-        web::http::experimental::listener::http_listener query_listener(web::http::experimental::listener::make_listener_uri(nmos::fields::query_port(registry_model.settings)), listener_config);
-        nmos::support_api(query_listener, query_api, gate);
+        port_routers[nmos::fields::query_port(registry_model.settings)].mount({}, nmos::make_query_api(registry_model, gate));
 
         // "Source ID of the Query API instance issuing the data Grain"
         // See https://github.com/AMWA-TV/nmos-discovery-registration/blob/v1.2/APIs/schemas/queryapi-subscriptions-websocket.json
@@ -152,18 +143,14 @@ int main(int argc, char* argv[])
 
         // Configure the Registration API
 
-        web::http::experimental::listener::api_router registration_api = nmos::make_registration_api(registry_model, gate);
-        web::http::experimental::listener::http_listener registration_listener(web::http::experimental::listener::make_listener_uri(nmos::fields::registration_port(registry_model.settings)), listener_config);
-        nmos::support_api(registration_listener, registration_api, gate);
+        port_routers[nmos::fields::registration_port(registry_model.settings)].mount({}, nmos::make_registration_api(registry_model, gate));
 
         // Configure the Node API
 
-        web::http::experimental::listener::api_router node_api = nmos::make_node_api(registry_model, {}, gate);
-        web::http::experimental::listener::http_listener node_listener(web::http::experimental::listener::make_listener_uri(nmos::fields::node_port(registry_model.settings)), listener_config);
-        nmos::support_api(node_listener, node_api, gate);
+        port_routers[nmos::fields::node_port(registry_model.settings)].mount({}, nmos::make_node_api(registry_model, {}, gate));
 
         // set up the node resources
-        auto & self_resources = registry_model.node_resources;
+        auto& self_resources = registry_model.node_resources;
         nmos::experimental::insert_registry_resources(self_resources, registry_model.settings);
 
         // add the self resources to the Registration API resources
@@ -173,9 +160,16 @@ int main(int argc, char* argv[])
         // Configure the Admin UI
 
         const utility::string_t admin_filesystem_root = U("./admin");
-        web::http::experimental::listener::api_router admin_ui = nmos::experimental::make_admin_ui(admin_filesystem_root, gate);
-        web::http::experimental::listener::http_listener admin_listener(web::http::experimental::listener::make_listener_uri(nmos::experimental::fields::admin_port(registry_model.settings)));
-        nmos::support_api(admin_listener, admin_ui, gate);
+        port_routers[nmos::experimental::fields::admin_port(registry_model.settings)].mount({}, nmos::experimental::make_admin_ui(admin_filesystem_root, gate));
+
+        // Set up the listeners for each API port
+
+        // try to use the configured TCP listen backlog
+        web::http::experimental::listener::http_listener_config listener_config;
+        listener_config.set_backlog(nmos::fields::listen_backlog(registry_model.settings));
+
+        std::vector<web::http::experimental::listener::http_listener> port_listeners;
+        for (auto& port_router : port_routers) port_listeners.push_back(nmos::make_api_listener(port_router.first, port_router.second, listener_config, gate));
 
         // Configure the mDNS advertisements for our APIs
 
@@ -188,27 +182,18 @@ int main(int argc, char* argv[])
             nmos::experimental::register_service(*advertiser, nmos::service_types::node, registry_model.settings, nmos::make_txt_records(nmos::service_types::node));
         }
 
-        slog::log<slog::severities::info>(gate, SLOG_FLF) << "Preparing for connections";
-
-        // start up registry management before any NMOS APIs are open
+        // Start up registry management before any NMOS APIs are open
 
         auto send_query_ws_events = nmos::details::make_thread_guard([&] { nmos::send_query_ws_events_thread(query_ws_listener, registry_model, registry_websockets, gate); }, [&] { registry_model.controlled_shutdown(); });
         auto erase_expired_resources = nmos::details::make_thread_guard([&] { nmos::erase_expired_resources_thread(registry_model, gate); }, [&] { registry_model.controlled_shutdown(); });
 
-        // open in an order that means NMOS APIs don't expose references to others that aren't open yet
+        // Open the API ports
 
-        web::http::experimental::listener::http_listener_guard logging_guard(logging_listener);
-        web::http::experimental::listener::http_listener_guard settings_guard(settings_listener);
+        slog::log<slog::severities::info>(gate, SLOG_FLF) << "Preparing for connections";
 
-        web::http::experimental::listener::http_listener_guard node_guard(node_listener);
+        std::vector<web::http::experimental::listener::http_listener_guard> port_guards;
+        for (auto& port_listener : port_listeners) port_guards.push_back({ port_listener });
         web::websockets::experimental::listener::websocket_listener_guard query_ws_guard(query_ws_listener);
-        web::http::experimental::listener::http_listener_guard query_guard(query_listener);
-        web::http::experimental::listener::http_listener_guard registration_guard(registration_listener);
-
-        web::http::experimental::listener::http_listener_guard admin_guard(admin_listener);
-
-        web::http::experimental::listener::http_listener_guard mdns_guard(mdns_listener);
-
         mdns::service_advertiser_guard advertiser_guard(*advertiser);
 
         slog::log<slog::severities::info>(gate, SLOG_FLF) << "Ready for connections";
