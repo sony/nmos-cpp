8d756056e29dba250dcd91be92e61adf907c224b
jwpwh <john.pruitt@wheatstone.com>
2018-12-03 11:10:44 -0500

    Added #ifdef __cpluscplus to protect .c files from boost C++ header

--- Development/cpprest/details/boost_u_workaround.h
+++ Development/cpprest/details/boost_u_workaround.h
@@ -1,6 +1,8 @@
 // Workaround for conflict between the 'U' macro and some uses of U as e.g. a template parameter in Boost libraries
 #pragma once
+#ifdef __cplusplus
 #include "cpprest/details/push_undef_u.h"
 // See https://stackoverflow.com/questions/43245055/issue-with-boost-1-64-and-visual-studio-2017
 #include <boost/move/detail/type_traits.hpp>
 #include "cpprest/details/pop_u.h"
+#endif
