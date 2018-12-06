7739d0f56fde1a8f7407c6d89ae79b8d316f9c8d
jwpwh <john.pruitt@wheatstone.com>
2018-12-06 12:16:06 -0500

    Added prototypes for node_main_thread and node_initial_resources(). The node_main_thread can be called from an app. The node_initial_resources could be called by an app but would probably be replaced by real senders and receivers from the app.

--- Development/nmos-cpp-node/node_implementation.h
+++ Development/nmos-cpp-node/node_implementation.h
@@ -1,6 +1,8 @@
 #ifndef NMOS_CPP_NODE_NODE_IMPLEMENTATION_H
 #define NMOS_CPP_NODE_NODE_IMPLEMENTATION_H
 
+#include "nmos/id.h"
+
 namespace slog
 {
     class base_gate;
@@ -11,9 +13,15 @@ namespace nmos
     struct node_model;
 }
 
+// This function exists so that the main() does not have to be included and collide with the apps main()
+int node_main_thread(int argc, char* argv[]);
+
 // This is an example of how to integrate the nmos-cpp library with a device-specific underlying implementation.
 // It constructs and inserts a node resource and some sub-resources into the model, based on the model settings,
 // and then waits for sender/receiver activations or shutdown.
 void node_implementation_thread(nmos::node_model& model, slog::base_gate& gate);
+
+// sample implementation function to create initial resources
+void node_initial_resources(nmos::node_model& model, const utility::string_t& seed_id, slog::base_gate& gate, nmos::id node_id = nmos::id());
 
 #endif
