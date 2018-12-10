14d1ec57a6f0bf0a56145275be3a7f5e86c60239
garethsb-sony <gareth.sylvester-bradley@sony.com>
2018-12-10 14:22:48 +0000

    Add utility functions to access the underlying storage of a web::json::object or web::json::array, for use when the limited interface exposed just isn't enough
    (cherry picked from commit 989e5361dd331e4caa77df6205d6ed7dccc18a91)

--- Development/cpprest/json_utils.h
+++ Development/cpprest/json_utils.h
@@ -290,6 +290,18 @@ namespace web
         {
             return value.at(value.size() - 1);
         }
+
+        namespace details
+        {
+            typedef std::vector<json::value> array_storage_t;
+            typedef std::vector<std::pair<utility::string_t, json::value>> object_storage_t;
+        }
+
+        // take care, but sometimes the limited interface just isn't enough
+        details::array_storage_t& storage_of(web::json::array& array);
+
+        // take care, but sometimes the limited interface just isn't enough
+        details::object_storage_t& storage_of(web::json::object& object);
     }
 }
 
