b317a981f7ad0e167290702339569eb6b697499c
garethsb-sony <gareth.sylvester-bradley@sony.com>
2019-04-17 10:08:43 +0100

    Allow configuration of the cipher list in a more idiomatic way, to avoid using SSL_CTX_set_cipher_list directly in user code
    (cherry picked from commit 809c156f7c93de8a6bdf5f102b22432b3fb9b9b0)

---
+++ Development/boost/asio/ssl/set_cipher_list.hpp
@@ -0,0 +1,48 @@
+#ifndef BOOST_ASIO_SSL_SET_CIPHER_LIST_HPP
+#define BOOST_ASIO_SSL_SET_CIPHER_LIST_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+
+#include <boost/asio/ssl/context.hpp>
+
+#include <boost/asio/detail/push_options.hpp>
+
+namespace boost {
+namespace asio {
+namespace ssl {
+
+inline
+BOOST_ASIO_SYNC_OP_VOID set_cipher_list(boost::asio::ssl::context& ctx,
+    const std::string& cipher_list, boost::system::error_code& ec)
+{
+    ::ERR_clear_error();
+
+    if (::SSL_CTX_set_cipher_list(ctx.native_handle(), cipher_list.c_str()) != 1)
+    {
+        ec = boost::system::error_code(
+            static_cast<int>(::ERR_get_error()),
+            boost::asio::error::get_ssl_category());
+        BOOST_ASIO_SYNC_OP_VOID_RETURN(ec);
+    }
+
+    ec = boost::system::error_code();
+    BOOST_ASIO_SYNC_OP_VOID_RETURN(ec);
+}
+
+inline
+void set_cipher_list(boost::asio::ssl::context& ctx, const std::string& cipher_list)
+{
+    boost::system::error_code ec;
+    set_cipher_list(ctx, cipher_list, ec);
+    boost::asio::detail::throw_error(ec, "set_cipher_list");
+}
+
+} // namespace ssl
+} // namespace asio
+} // namespace boost
+
+#include <boost/asio/detail/pop_options.hpp>
+
+#endif // BOOST_ASIO_SSL_SET_CIPHER_LIST_HPP
