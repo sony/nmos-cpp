sony/nmos-cpp

commit 4a70665cb750d24b4aff8a38a93deb519e2ca45f

Improved and clarified exception handling for the Connection API and connection activation callbacks
(cherry picked from commit 55f1fea82b19a8c6430ab7053f116c2a06459bf5)

Development/nmos/connection_api.cpp
@@ -778,17 +778,24 @@ namespace nmos
         auto& staged_activation = staged[nmos::fields::activation];
         const nmos::activation_mode staged_mode{ nmos::fields::mode(staged_activation).as_string() };
 
+        auto& active = nmos::fields::endpoint_active(resource.data);
+
+        // "On activation all instances of "auto" must be resolved into the actual values that will be used, unless
+        // there is an error condition. If there is an error condition that means `auto` cannot be resolved, the active
+        // transport parameters must not change, and the underlying sender must continue as before."
+        // Therefore, in case it throws an exception, resolve_auto is called on a copy of the /staged resource data,
+        // before making any changes to the /active resource data.
+        // See https://github.com/AMWA-TV/nmos-device-connection-management/blob/v1.1-dev/APIs/ConnectionAPI.raml#L280-L289
+        // and https://github.com/AMWA-TV/nmos-device-connection-management/blob/v1.1-dev/docs/2.2.%20APIs%20-%20Server%20Side%20Implementation.md#use-of-auto
+        auto activating = staged;
+        resolve_auto(activating);
+
         // Set the time of activation (will be included in the PATCH response for an immediate activation)
         staged_activation[nmos::fields::activation_time] = at;
 
         // "When a set of 'staged' settings is activated, these settings transition into the 'active' resource."
         // See https://github.com/AMWA-TV/nmos-device-connection-management/blob/v1.0/docs/1.0.%20Overview.md#active
-        resource.data[nmos::fields::endpoint_active] = resource.data[nmos::fields::endpoint_staged];
-
-        // "On activation all instances of "auto" should be resolved into the actual values that will be used"
-        // See https://github.com/AMWA-TV/nmos-device-connection-management/blob/v1.0/APIs/ConnectionAPI.raml#L257
-        // and https://github.com/AMWA-TV/nmos-device-connection-management/blob/v1.0/docs/2.2.%20APIs%20-%20Server%20Side%20Implementation.md#use-of-auto
-        resolve_auto(resource.data[nmos::fields::endpoint_active]);
+        active = activating;
 
         // Unclear whether the activation in the active endpoint should have values for mode, requested_time
         // (and even activation_time?) or whether they should be null? The examples have them with values.
@@ -1004,7 +1011,7 @@ namespace nmos
 
                 const auto type = nmos::type_from_resourceType(resourceType);
 
-                const auto handle_connection_resource_exception = [&](const nmos::id& id)
+                const auto handle_connection_resource_exception = [&](const std::pair<nmos::id, nmos::type>& id_type)
                 {
                     // try-catch based on the exception handler in nmos::add_api_finally_handler
                     try
@@ -1013,32 +1020,32 @@ namespace nmos
                     }
                     catch (const web::json::json_exception& e)
                     {
-                        slog::log<slog::severities::warning>(gate, SLOG_FLF) << "JSON error for " << id << " in bulk request: " << e.what();
+                        slog::log<slog::severities::warning>(gate, SLOG_FLF) << "JSON error for " << id_type << " in bulk request: " << e.what();
                         return details::make_connection_resource_patch_error_response(status_codes::BadRequest, e);
                     }
                     catch (const web::http::http_exception& e)
                     {
-                        slog::log<slog::severities::warning>(gate, SLOG_FLF) << "HTTP error for " << id << " in bulk request: " << e.what() << " [" << e.error_code() << "]";
+                        slog::log<slog::severities::warning>(gate, SLOG_FLF) << "HTTP error for " << id_type << " in bulk request: " << e.what() << " [" << e.error_code() << "]";
                         return details::make_connection_resource_patch_error_response(status_codes::BadRequest, e);
                     }
                     catch (const std::runtime_error& e)
                     {
-                        slog::log<slog::severities::error>(gate, SLOG_FLF) << "Implementation error for " << id << " in bulk request: " << e.what();
+                        slog::log<slog::severities::error>(gate, SLOG_FLF) << "Implementation error for " << id_type << " in bulk request: " << e.what();
                         return details::make_connection_resource_patch_error_response(status_codes::NotImplemented, e);
                     }
                     catch (const std::logic_error& e)
                     {
-                        slog::log<slog::severities::error>(gate, SLOG_FLF) << "Implementation error for " << id << " in bulk request: " << e.what();
+                        slog::log<slog::severities::error>(gate, SLOG_FLF) << "Implementation error for " << id_type << " in bulk request: " << e.what();
                         return details::make_connection_resource_patch_error_response(status_codes::InternalError, e);
                     }
                     catch (const std::exception& e)
                     {
-                        slog::log<slog::severities::error>(gate, SLOG_FLF) << "Unexpected exception for " << id << " in bulk request: " << e.what();
+                        slog::log<slog::severities::error>(gate, SLOG_FLF) << "Unexpected exception for " << id_type << " in bulk request: " << e.what();
                         return details::make_connection_resource_patch_error_response(status_codes::InternalError, e);
                     }
                     catch (...)
                     {
-                        slog::log<slog::severities::severe>(gate, SLOG_FLF) << "Unexpected unknown exception for " << id << " in bulk request";
+                        slog::log<slog::severities::severe>(gate, SLOG_FLF) << "Unexpected unknown exception for " << id_type << " in bulk request";
                         return details::make_connection_resource_patch_error_response(status_codes::InternalError);
                     }
                 };
@@ -1055,7 +1062,7 @@ namespace nmos
                     }
                     catch (...)
                     {
-                        result = handle_connection_resource_exception(id);
+                        result = handle_connection_resource_exception({ id, type });
                     }
 
                     results.push_back(result);
@@ -1084,7 +1091,7 @@ namespace nmos
                             }
                             catch (...)
                             {
-                                result = handle_connection_resource_exception(id);
+                                result = handle_connection_resource_exception({ id, type });
                             }
                         }
                     }

Development/nmos/connection_activation.cpp
@@ -94,37 +94,78 @@ namespace nmos
 
                 // Update the IS-05 connection resource
 
-                nmos::modify_resource(model.connection_resources, resource.id, [&](nmos::resource& connection_resource)
+                // ensure nmos::set_connection_resource_not_pending is called to 'unlock' the resource for the Connection API implementation
+                // after an exception; for immediate activations this will cause a 500 Internal Error status code to be returned
+                const auto handle_connection_activation_exception = [&](const std::pair<nmos::id, nmos::type>& id_type)
                 {
-                    const std::pair<nmos::id, nmos::type> id_type{ connection_resource.id, connection_resource.type };
-                    auto matching_resource = find_resource(model.node_resources, id_type);
-                    if (model.node_resources.end() == matching_resource)
+                    // try-catch based on the exception handler in nmos::add_api_finally_handler
+                    try
                     {
-                        slog::log<slog::severities::error>(gate, SLOG_FLF) << "Implementation error for " << id_type << " during activation: " << "matching IS-04 resource not found";
-                        return;
+                        throw;
+                    }
+                    catch (const web::json::json_exception& e)
+                    {
+                        slog::log<slog::severities::error>(gate, SLOG_FLF) << "JSON error for " << id_type << " during activation: " << e.what();
+                    }
+                    catch (const std::runtime_error& e)
+                    {
+                        slog::log<slog::severities::error>(gate, SLOG_FLF) << "Implementation error for " << id_type << " during activation: " << e.what();
+                    }
+                    catch (const std::logic_error& e)
+                    {
+                        slog::log<slog::severities::error>(gate, SLOG_FLF) << "Implementation error for " << id_type << " during activation: " << e.what();
+                    }
+                    catch (const std::exception& e)
+                    {
+                        slog::log<slog::severities::error>(gate, SLOG_FLF) << "Unexpected exception for " << id_type << " during activation: " << e.what();
+                    }
+                    catch (...)
+                    {
+                        slog::log<slog::severities::severe>(gate, SLOG_FLF) << "Unexpected unknown exception for " << id_type << " during activation";
                     }
 
-                    nmos::set_connection_resource_active(connection_resource, [&](web::json::value& endpoint_active)
+                    nmos::modify_resource(model.connection_resources, id_type.first, [&](nmos::resource& connection_resource)
                     {
-                        resolve_auto(*matching_resource, connection_resource, nmos::fields::transport_params(endpoint_active));
-                        active = nmos::fields::master_enable(endpoint_active);
-                        // Senders indicate the connected receiver_id, receivers indicate the connected sender_id
-                        auto& connected_id_or_null = nmos::types::sender == id_type.second ? nmos::fields::receiver_id(endpoint_active) : nmos::fields::sender_id(endpoint_active);
-                        if (!connected_id_or_null.is_null()) connected_id = connected_id_or_null.as_string();
-                    }, activation_time);
+                        nmos::set_connection_resource_not_pending(connection_resource);
+                    });
+                };
 
-                    if (nmos::types::sender == id_type.second)
-                    {
-                        set_transportfile(*matching_resource, connection_resource, connection_resource.data[nmos::fields::endpoint_transportfile]);
-                    }
-                });
-
-                // Update the IS-04 resource
-
-                nmos::modify_resource(model.node_resources, resource.id, [&activation_time, &active, &connected_id](nmos::resource& resource)
+                try
                 {
-                    nmos::set_resource_subscription(resource, active, connected_id, activation_time);
-                });
+                    nmos::modify_resource(model.connection_resources, id_type.first, [&](nmos::resource& connection_resource)
+                    {
+                        auto matching_resource = find_resource(model.node_resources, id_type);
+                        if (model.node_resources.end() == matching_resource)
+                        {
+                            throw std::logic_error("matching IS-04 resource not found");
+                        }
+
+                        nmos::set_connection_resource_active(connection_resource, [&](web::json::value& endpoint_active)
+                        {
+                            resolve_auto(*matching_resource, connection_resource, nmos::fields::transport_params(endpoint_active));
+                            active = nmos::fields::master_enable(endpoint_active);
+                            // Senders indicate the connected receiver_id, receivers indicate the connected sender_id
+                            auto& connected_id_or_null = nmos::types::sender == id_type.second ? nmos::fields::receiver_id(endpoint_active) : nmos::fields::sender_id(endpoint_active);
+                            if (!connected_id_or_null.is_null()) connected_id = connected_id_or_null.as_string();
+                        }, activation_time);
+
+                        if (nmos::types::sender == id_type.second)
+                        {
+                            set_transportfile(*matching_resource, connection_resource, connection_resource.data[nmos::fields::endpoint_transportfile]);
+                        }
+                    });
+
+                    // Update the IS-04 resource
+
+                    nmos::modify_resource(model.node_resources, id_type.first, [&activation_time, &active, &connected_id](nmos::resource& resource)
+                    {
+                        nmos::set_resource_subscription(resource, active, connected_id, activation_time);
+                    });
+                }
+                catch (...)
+                {
+                    handle_connection_activation_exception(id_type);
+                }
 
                 notify = true;
             });

Development/nmos/connection_activation.h
@@ -22,9 +22,11 @@ namespace nmos
     struct resource;
 
     // a connection_resource_auto_resolver overwrites every instance of "auto" in the specified transport_params array for the specified (IS-04/IS-05) sender/connection_sender or receiver/connection_receiver
+    // it may throw e.g. web::json::json_exception or std::runtime_error, which will prevent activation and for immediate activations cause a 500 Internal Error status code to be returned
     typedef std::function<void(const nmos::resource&, const nmos::resource&, web::json::value&)> connection_resource_auto_resolver;
 
     // a connection_sender_transportfile_setter updates the specified /transportfile endpoint for the specified (IS-04/IS-05) sender/connection_sender
+    // it may throw e.g. web::json::json_exception or std::runtime_error, which will prevent activation and for immediate activations cause a 500 Internal Error status code to be returned
     typedef std::function<void(const nmos::resource&, const nmos::resource&, web::json::value&)> connection_sender_transportfile_setter;
 
     void connection_activation_thread(nmos::node_model& model, connection_resource_auto_resolver resolve_auto, connection_sender_transportfile_setter set_transportfile, slog::base_gate& gate);

Development/nmos/connection_api.h
@@ -10,7 +10,7 @@ namespace slog
 }
 
 // Connection API implementation
-// See https://github.com/AMWA-TV/nmos-device-connection-management/blob/v1.0/APIs/ConnectionAPI.raml
+// See https://github.com/AMWA-TV/nmos-device-connection-management/blob/v1.1-dev/APIs/ConnectionAPI.raml
 namespace nmos
 {
     struct api_version;
@@ -19,7 +19,10 @@ namespace nmos
     struct tai;
     struct type;
 
+    // Connection API callbacks
+
     // a transport_file_parser validates the specified transport file type/data for the specified (IS-04/IS-05) resource/connection_resource and returns a transport_params array to be merged
+    // or may throw std::runtime_error, which will be mapped to a 500 Internal Error status code with NMOS error "debug" information including the exception message
     // (the default transport file parser only supports RTP transport via the default SDP parser)
     typedef std::function<web::json::value(const nmos::resource&, const nmos::resource&, const utility::string_t&, const utility::string_t&, slog::base_gate&)> transport_file_parser;
 
@@ -27,8 +30,11 @@ namespace nmos
     {
         // a connection_resource_patch_validator can be used to perform any final validation of the specified merged /staged value for the specified (IS-04/IS-05) resource/connection_resource
         // that cannot be expressed by the schemas or /constraints endpoint
+        // it may throw web::json::json_exception, which will be mapped to a 400 Bad Request status code with NMOS error "debug" information including the exception message
         typedef std::function<void(const nmos::resource&, const nmos::resource&, const web::json::value&, slog::base_gate&)> connection_resource_patch_validator;
     }
+
+    // Connection API factory functions
 
     web::http::experimental::listener::api_router make_connection_api(nmos::node_model& model, transport_file_parser parse_transport_file, details::connection_resource_patch_validator validate_merged, slog::base_gate& gate);
 
@@ -39,10 +45,13 @@ namespace nmos
 
     web::http::experimental::listener::api_router make_connection_api(nmos::node_model& model, slog::base_gate& gate);
 
+    // Connection API implementation details shared with the Node API /receivers/{receiverId}/target endpoint
     namespace details
     {
         void handle_connection_resource_patch(web::http::http_response res, nmos::node_model& model, const nmos::api_version& version, const std::pair<nmos::id, nmos::type>& id_type, const web::json::value& patch, transport_file_parser parse_transport_file, connection_resource_patch_validator validate_merged, slog::base_gate& gate);
     }
+
+    // Functions for interaction between the Connection API implementation and the connection activation thread
 
     // Activate an IS-05 sender or receiver by transitioning the 'staged' settings into the 'active' resource
     void set_connection_resource_active(nmos::resource& connection_resource, std::function<void(web::json::value&)> resolve_auto, const nmos::tai& activation_time);
@@ -55,7 +64,10 @@ namespace nmos
     // (This function should be called after nmos::set_connection_resource_active.)
     void set_resource_subscription(nmos::resource& node_resource, bool active, const nmos::id& connected_id, const nmos::tai& activation_time);
 
+    // Helper functions for the Connection API callbacks
+
     // Validate and parse the specified transport file for the specified receiver
+    // (this is the default transport file parser)
     web::json::value parse_rtp_transport_file(const nmos::resource& receiver, const nmos::resource& connection_receiver, const utility::string_t& transport_file_type, const utility::string_t& transport_file_data, slog::base_gate& gate);
 
     // "On activation all instances of "auto" should be resolved into the actual values that will be used"

commit 256a992ee37ed1019988eb8e93a1731dcf5342f3

Fix bug that "activation_time" would not be set in the /active endpoint introduced by commit 4a70665cb750d24b4aff8a38a93deb519e2ca45f
(cherry picked from commit 82550d5e50113331eaf089e05ce62bd25f49af8c)

Development/nmos/connection_api.cpp
@@ -826,6 +826,9 @@ namespace nmos
         auto& staged_activation = staged[nmos::fields::activation];
         const nmos::activation_mode staged_mode{ nmos::fields::mode(staged_activation).as_string() };
 
+        // Set the time of activation (will be included in the PATCH response for an immediate activation)
+        staged_activation[nmos::fields::activation_time] = at;
+
         auto& active = nmos::fields::endpoint_active(resource.data);
 
         // "On activation all instances of "auto" must be resolved into the actual values that will be used, unless
@@ -837,9 +840,6 @@ namespace nmos
         // and https://github.com/AMWA-TV/nmos-device-connection-management/blob/v1.1-dev/docs/2.2.%20APIs%20-%20Server%20Side%20Implementation.md#use-of-auto
         auto activating = staged;
         resolve_auto(activating);
-
-        // Set the time of activation (will be included in the PATCH response for an immediate activation)
-        staged_activation[nmos::fields::activation_time] = at;
 
         // "When a set of 'staged' settings is activated, these settings transition into the 'active' resource."
         // See https://github.com/AMWA-TV/nmos-device-connection-management/blob/v1.0/docs/1.0.%20Overview.md#active