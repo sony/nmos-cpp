edd896214a772b10bbcfae89824bfeb7b22743a4
garethsb-sony <gareth.sylvester-bradley@sony.com>
2020-01-21 17:11:07 +0000

    [#108] Allow CMAKE_CXX_STANDARD to be overridden by the CMake user e.g. to allow C++17 to be selected
    (cherry picked from commit 013943feb264415b33a601bbe1a4f16b6d07a2de)

--- Development/cmake/NmosCppCommon.cmake
+++ Development/cmake/NmosCppCommon.cmake
@@ -11,7 +11,10 @@ set (SLOG_LOGGING_SEVERITY slog::max_verbosity CACHE STRING "Compile-time loggin
 
 # enable C++11
 enable_language(CXX)
-set(CMAKE_CXX_STANDARD 11)
+set(CMAKE_CXX_STANDARD 11 CACHE STRING "Default value for CXX_STANDARD property of targets")
+if(CMAKE_CXX_STANDARD STREQUAL "98")
+    message(FATAL_ERROR "CMAKE_CXX_STANDARD must be 11 or higher; C++98 is not supported")
+endif()
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
