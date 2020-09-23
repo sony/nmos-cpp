823036e2f484daa967f62ac1ce0069b3dff83230
alanb-sony <alan.birtles@eu.sony.com>
2020-09-23 14:28:00 +0100

    Use nmos-testing UserConfig.py
    update testssl
    add option to run_nmos_testing.sh to specify secure or insecure
    fix not adding registry subscriptions in secure mode

--- Sandbox/run_nmos_testing.sh
+++ Sandbox/run_nmos_testing.sh
@@ -12,31 +12,41 @@ badges_dir=$1
 shift
 host_ip=$1
 shift
-bulid_prefix=$1
+build_prefix=$1
+shift
+secure_tests=$1
 shift
 
-ssl_params="\"client_secure\":true,\
-\"server_secure\":true,\
-\"ca_certificate_file\":\"test_data/BCP00301/ca/certs/ca.cert.pem\",\
-\"private_key_files\":[\
-  \"test_data/BCP00301/ca/intermediate/private/ecdsa.api.testsuite.nmos.tv.key.pem\",
-  \"test_data/BCP00301/ca/intermediate/private/rsa.api.testsuite.nmos.tv.key.pem\"],\
-\"certificate_chain_files\":[\
-  \"test_data/BCP00301/ca/intermediate/certs/ecdsa.api.testsuite.nmos.tv.cert.chain.pem\",\
-  \"test_data/BCP00301/ca/intermediate/certs/rsa.api.testsuite.nmos.tv.cert.chain.pem\"],\
-\"dh_param_file\":\"test_data/BCP00301/ca/intermediate/private/dhparam.pem\",
-\"host_name\":\"api.testsuite.nmos.tv\",
-\"host_address\":\"${host_ip}\"
-"
+if [[ "${secure_tests}" == "true" ]]; then
+  host=api.testsuite.nmos.tv
+  common_params=",\"client_secure\":true,\
+  \"server_secure\":true,\
+  \"ca_certificate_file\":\"test_data/BCP00301/ca/certs/ca.cert.pem\",\
+  \"private_key_files\":[\
+    \"test_data/BCP00301/ca/intermediate/private/ecdsa.api.testsuite.nmos.tv.key.pem\",
+    \"test_data/BCP00301/ca/intermediate/private/rsa.api.testsuite.nmos.tv.key.pem\"],\
+  \"certificate_chain_files\":[\
+    \"test_data/BCP00301/ca/intermediate/certs/ecdsa.api.testsuite.nmos.tv.cert.chain.pem\",\
+    \"test_data/BCP00301/ca/intermediate/certs/rsa.api.testsuite.nmos.tv.cert.chain.pem\"],\
+  \"dh_param_file\":\"test_data/BCP00301/ca/intermediate/private/dhparam.pem\",
+  \"host_name\":\"${host}\",
+  \"host_address\":\"${host_ip}\"
+  "
+  registry_url=https://${host}:8088
+else
+  host=${host_ip}
+  common_params=
+  registry_url=http://localhost:8088
+fi
 
-"${node_command}" "{\"how_many\":6,\"http_port\":1080,\"domain\":\"local.\",\"logging_level\":-40, ${ssl_params}}" > ${results_dir}/nodeoutput 2>&1 &
+"${node_command}" "{\"how_many\":6,\"http_port\":1080,\"domain\":\"local.\",\"logging_level\":-40 ${common_params}}" > ${results_dir}/nodeoutput 2>&1 &
 NODE_PID=$!
 
 function do_run_test() {
   suite=$1
   echo "Running $suite"
   shift
-  output_file=${results_dir}/${bulid_prefix}${suite}.json
+  output_file=${results_dir}/${build_prefix}${suite}.json
   result=$(${run_test} suite ${suite} --selection all "$@" --output "${output_file}" >> ${results_dir}/testoutput 2>&1; echo $?)
   if [ ! -e ${output_file} ]; then
     echo "No output produced"
@@ -48,37 +58,39 @@ function do_run_test() {
   esac
 }
 
-do_run_test BCP-003-01 --host "api.testsuite.nmos.tv" --port 1080 --version v1.0
+if [[ "${secure_tests}" == "true" ]]; then
+  do_run_test BCP-003-01 --host "${host}" --port 1080 --version v1.0
+fi
 
-do_run_test IS-04-01 --host "api.testsuite.nmos.tv" --port 1080 --version v1.3
+do_run_test IS-04-01 --host "${host}" --port 1080 --version v1.3
 
-do_run_test IS-04-03 --host "api.testsuite.nmos.tv" --port 1080 --version v1.3
+do_run_test IS-04-03 --host "${host}" --port 1080 --version v1.3
 
-do_run_test IS-05-01 --host "api.testsuite.nmos.tv" --port 1080 --version v1.1
+do_run_test IS-05-01 --host "${host}" --port 1080 --version v1.1
 
-do_run_test IS-05-02 --host "api.testsuite.nmos.tv" "api.testsuite.nmos.tv" --port 1080 1080 --version v1.3 v1.1
+do_run_test IS-05-02 --host "${host}" "${host}" --port 1080 1080 --version v1.3 v1.1
 
-do_run_test IS-07-01 --host "api.testsuite.nmos.tv" --port 1080 --version v1.0
+do_run_test IS-07-01 --host "${host}" --port 1080 --version v1.0
 
-do_run_test IS-07-02 --host "api.testsuite.nmos.tv" "api.testsuite.nmos.tv" "api.testsuite.nmos.tv" --port 1080 1080 1080 --version v1.3 v1.1 v1.0
+do_run_test IS-07-02 --host "${host}" "${host}" "${host}" --port 1080 1080 1080 --version v1.3 v1.1 v1.0
 
-do_run_test IS-08-01 --host "api.testsuite.nmos.tv" --port 1080 --version v1.0 --selector null
+do_run_test IS-08-01 --host "${host}" --port 1080 --version v1.0 --selector null
 
-do_run_test IS-08-02 --host "api.testsuite.nmos.tv" "api.testsuite.nmos.tv" --port 1080 1080 --version v1.3 v1.0 --selector null null
+do_run_test IS-08-02 --host "${host}" "${host}" --port 1080 1080 --version v1.3 v1.0 --selector null null
 
-do_run_test IS-09-02 --host "api.testsuite.nmos.tv" null --port 0 0 --version null v1.0
+do_run_test IS-09-02 --host "${host}" null --port 0 0 --version null v1.0
 
 # Run Registry tests (leave Node running)
-"${registry_command}" "{\"pri\":0,\"http_port\":8088,\"domain\":\"local.\",\"logging_level\":-40, ${ssl_params}}" > ${results_dir}/registryoutput 2>&1 &
+"${registry_command}" "{\"pri\":0,\"http_port\":8088,\"domain\":\"local.\",\"logging_level\":-40 ${common_params}}" > ${results_dir}/registryoutput 2>&1 &
 REGISTRY_PID=$!
 # short delay to give the Registry a chance to start up and the Node a chance to register before running the Registry test suite
 sleep 2
 # add a persistent Query WebSocket API subscription before running the Registry test suite
-curl "http://localhost:8088/x-nmos/query/v1.3/subscriptions" -H "Content-Type: application/json" -d "{\"max_update_rate_ms\": 100, \"resource_path\": \"/nodes\", \"params\": {\"label\": \"host1\"}, \"persist\": true, \"secure\": false}"
+curl "${registry_url}/x-nmos/query/v1.3/subscriptions" -H "Content-Type: application/json" -d "{\"max_update_rate_ms\": 100, \"resource_path\": \"/nodes\", \"params\": {\"label\": \"host1\"}, \"persist\": true, \"secure\": false}" || echo "failed to add subscription"
 
-do_run_test IS-04-02 --host "api.testsuite.nmos.tv" "api.testsuite.nmos.tv" --port 8088 8088 --version v1.3 v1.3
+do_run_test IS-04-02 --host "${host}" "${host}" --port 8088 8088 --version v1.3 v1.3
 
-do_run_test IS-09-01 --host "api.testsuite.nmos.tv" --port 8088 --version v1.0
+do_run_test IS-09-01 --host "${host}" --port 8088 --version v1.0
 
 # Stop Node and Registry
 kill $NODE_PID || echo "node not running"
