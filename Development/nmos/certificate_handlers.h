17f3e3c92069447d0468c35a07fa90a678c65375
Simon Lo <simon.lo@sony.com>
2021-04-27 14:57:55 +0100

    Update Development/nmos/certificate_handlers.h
    Co-authored-by: Gareth Sylvester-Bradley <31761158+garethsb@users.noreply.github.com>

--- Development/nmos/certificate_handlers.h
+++ Development/nmos/certificate_handlers.h
@@ -16,7 +16,7 @@ namespace nmos
     // this callback should not throw exceptions
     typedef std::function<std::pair<utility::string_t, utility::string_t>()> load_cert_handler;
 
-    // callback to supply Diffie-Hellman parameters for ephemeral key exchange support
+    // callback to supply Diffie-Hellman parameters for ephemeral key exchange support, in PEM format
     // see e.g. https://wiki.openssl.org/index.php/Diffie-Hellman_parameters
     // this callback should not throw exceptions
     typedef std::function<utility::string_t()> load_dh_param_handler;
