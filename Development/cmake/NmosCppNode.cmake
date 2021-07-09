0e21566611e0bbec911981603728fb5a8ca0bed1
Gareth Sylvester-Bradley <garethsb@gmail.com>
2021-07-09 13:08:34 +0100

    Move nmos-cpp-node and nmos-cpp-registry into their own cmake files

---
+++ Development/cmake/NmosCppNode.cmake
@@ -0,0 +1,41 @@
+# caller can set NMOS_CPP_DIR if the project is different
+if (NOT DEFINED NMOS_CPP_DIR)
+    set (NMOS_CPP_DIR ${PROJECT_SOURCE_DIR})
+endif()
+
+# nmos-cpp-node executable
+
+set(NMOS_CPP_NODE_SOURCES
+    ${NMOS_CPP_DIR}/nmos-cpp-node/main.cpp
+    ${NMOS_CPP_DIR}/nmos-cpp-node/node_implementation.cpp
+    )
+set(NMOS_CPP_NODE_HEADERS
+    ${NMOS_CPP_DIR}/nmos-cpp-node/node_implementation.h
+    )
+
+add_executable(
+    nmos-cpp-node
+    ${NMOS_CPP_NODE_SOURCES}
+    ${NMOS_CPP_NODE_HEADERS}
+    ${NMOS_CPP_DIR}/nmos-cpp-node/config.json
+    )
+
+source_group("Source Files" FILES ${NMOS_CPP_NODE_SOURCES})
+source_group("Header Files" FILES ${NMOS_CPP_NODE_HEADERS})
+
+target_link_libraries(
+    nmos-cpp-node
+    nmos-cpp_static
+    ${CPPRESTSDK_TARGET}
+    ${PLATFORM_LIBS}
+    ${Boost_LIBRARIES}
+    )
+
+if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
+    # Conan packages usually don't include PDB files so suppress the resulting warning
+    set_target_properties(
+        nmos-cpp-node
+        PROPERTIES
+        LINK_FLAGS "/ignore:4099"
+        )
+endif()
---
+++ Development/cmake/NmosCppRegistry.cmake
@@ -0,0 +1,41 @@
+# caller can set NMOS_CPP_DIR if the project is different
+if (NOT DEFINED NMOS_CPP_DIR)
+    set (NMOS_CPP_DIR ${PROJECT_SOURCE_DIR})
+endif()
+
+# nmos-cpp-registry executable
+
+set(NMOS_CPP_REGISTRY_SOURCES
+    ${NMOS_CPP_DIR}/nmos-cpp-registry/main.cpp
+    ${NMOS_CPP_DIR}/nmos-cpp-registry/registry_implementation.cpp
+    )
+set(NMOS_CPP_REGISTRY_HEADERS
+    ${NMOS_CPP_DIR}/nmos-cpp-registry/registry_implementation.h
+    )
+
+add_executable(
+    nmos-cpp-registry
+    ${NMOS_CPP_REGISTRY_SOURCES}
+    ${NMOS_CPP_REGISTRY_HEADERS}
+    ${NMOS_CPP_DIR}/nmos-cpp-registry/config.json
+    )
+
+source_group("Source Files" FILES ${NMOS_CPP_REGISTRY_SOURCES})
+source_group("Header Files" FILES ${NMOS_CPP_REGISTRY_HEADERS})
+
+target_link_libraries(
+    nmos-cpp-registry
+    nmos-cpp_static
+    ${CPPRESTSDK_TARGET}
+    ${PLATFORM_LIBS}
+    ${Boost_LIBRARIES}
+    )
+
+if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
+    # Conan packages usually don't include PDB files so suppress the resulting warning
+    set_target_properties(
+        nmos-cpp-registry
+        PROPERTIES
+        LINK_FLAGS "/ignore:4099"
+        )
+endif()
