6e0f8a55211cb5cb32cc0b4e235a052e6852ee41
Gareth Sylvester-Bradley <garethsb@gmail.com>
2021-08-09 09:53:41 +0100

    Add explanation in comments

--- Sandbox/conan-recipe/test_package/conanfile.py
+++ Sandbox/conan-recipe/test_package/conanfile.py
@@ -5,12 +5,13 @@ from conans import ConanFile, CMake, tools
 
 class NmosCppTestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "cmake_find_package"
+    # use cmake_find_package_multi because the project installs a config-file package
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
-        # hmm, why does CMake report e.g. "Library nmos-cpp not found in package, might be system one"?
+        # hmm, on Windows why does CMake report e.g. "Library nmos-cpp not found in package, might be system one"?
         # yet it still works...
         cmake.build()
 
