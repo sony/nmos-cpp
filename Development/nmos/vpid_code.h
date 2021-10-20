a3f2b1216a58de445088885368d5d10ad7a50db6
Gareth Sylvester-Bradley <garethsb@nvidia.com>
2021-10-20 09:39:44 +0100

    Update Video Payload IP Codes from https://smpte-ra.org/video-payload-id-codes-serial-digital-interfaces

--- Development/nmos/vpid_code.h
+++ Development/nmos/vpid_code.h
@@ -13,14 +13,16 @@ namespace nmos
     // See https://smpte-ra.org/video-payload-id-codes-serial-digital-interfaces
     namespace vpid_codes
     {
-        // 483/576-line interlaced payloads on 270 Mb/s and 360 Mb/s serial digital interfaces
+        // 483/576-line interlaced video payloads on 270 Mb/s and 360 Mb/s serial digital interfaces
         const vpid_code vpid_270Mbps = 129;
-        // 483/576-line extended payloads on 360 Mb/s single-link and 270 Mb/s dual-link serial digital interfaces
+        // 483/576-line extended video payloads on 360 Mb/s single-link and 270 Mb/s dual-link serial digital interfaces
         const vpid_code vpid_360Mbps = 130;
-        // 483/576-line payloads on a 540 Mb/s serial digital interface
+        // 483/576-line video payloads on a 540 Mb/s serial digital interface
         const vpid_code vpid_540Mbps = 131;
-        // 483/576-line payloads on a 1.485 Gb/s (nominal) serial digital interface
-        const vpid_code vpid_1_5Gbps = 132;
+        // 720-line video payloads on a 1.5 Gb/s (nominal) serial digital interface
+        const vpid_code vpid_1_5Gbps_720_line = 132;
+        // 1080-line video payloads on a 1.5 Gb/s (nominal) serial digital interface
+        const vpid_code vpid_1_5Gbps_1080_line = 133;
 
         // extensible enum
     }
