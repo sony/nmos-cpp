dffece643584ebf18abfb4fb4a90325cae13aca1
lo-simon <simon.lo@sony.com>
2022-01-10 14:57:41 +0000

    Add a new test to ensure the last key value of the key_path is used for checking against

--- Development/rql/test/rql_test.cpp
+++ Development/rql/test/rql_test.cpp
@@ -12,7 +12,8 @@ BST_TEST_CASE(testRqlParse)
         { U("foo"), web::json::value_of({
             { U("bar"), web::json::value_of({
                 { U("baz.qux"), U("quux") },
-                { U("baz%2Equx"), U("quuux") }
+                { U("baz%2Equx"), U("quuux") },
+                { U("foo"), U("quuuux") }
             }) }
         }) }
     });
@@ -37,5 +38,16 @@ BST_TEST_CASE(testRqlParse)
         web::json::value results;
         BST_REQUIRE_EQUAL(true, web::json::extract(test_value.as_object(), results, key_path));
         BST_REQUIRE_EQUAL(U("quuux"), results.as_string());
+    }
+
+    {
+        const utility::string_t query_rql = U("matches(foo.bar.foo,quuuux)");
+        const auto rql_query = rql::parse_query(query_rql, false);
+
+        const auto key_path = nmos::split_decode_key_path(rql_query.at(U("args")).at(0).as_string());
+
+        web::json::value results;
+        BST_REQUIRE_EQUAL(true, web::json::extract(test_value.as_object(), results, key_path));
+        BST_REQUIRE_EQUAL(U("quuuux"), results.as_string());
     }
 }
