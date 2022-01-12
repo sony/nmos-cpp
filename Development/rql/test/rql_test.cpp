ef8def53b8e94ae9dae5aeb71c2a6c8002d80602
Gareth Sylvester-Bradley <garethsb@nvidia.com>
2022-01-12 14:55:33 +0000

    Simplify tests for rql::parse_query now that there really aren't any changes and there are explicit tests for web::json::extract

--- Development/rql/test/rql_test.cpp
+++ Development/rql/test/rql_test.cpp
@@ -1,58 +1,36 @@
 // The first "test" is of course whether the header compiles standalone
 #include "rql/rql.h"
 
-#include <boost/range/adaptor/transformed.hpp>
 #include "bst/test/test.h"
-#include "cpprest/json_utils.h"
 
 ////////////////////////////////////////////////////////////////////////////////////////////
-BST_TEST_CASE(testRqlParse)
+BST_TEST_CASE(testRqlParseQuery)
 {
-    const auto test_value = web::json::value_of({
-        { U("foo"), web::json::value_of({
-            { U("bar"), web::json::value_of({
-                { U("baz.qux"), U("quux") },
-                { U("baz%2Equx"), U("quuux") },
-                { U("foo"), U("quuuux") }
-            }) }
-        }) }
-    });
-
-    auto get_key_path = [](const web::json::array& key)
     {
-        return boost::copy_range<std::vector<utility::string_t>>(key | boost::adaptors::transformed([](const web::json::value& key_path)
-        {
-            return key_path.as_string();
-        }));
-    };
-
-    {
-        const utility::string_t query_rql = U("eq((foo,bar,baz.qux),quux)");
+        const utility::string_t query_rql = U("eq(foo.bar.baz.qux,meow)");
         const auto rql_query = rql::parse_query(query_rql);
-        const auto key_path = get_key_path(rql_query.at(U("args")).at(0).as_array());
-
-        web::json::value results;
-        BST_REQUIRE_EQUAL(true, web::json::extract(test_value.as_object(), results, key_path));
-        BST_REQUIRE_EQUAL(U("quux"), results.as_string());
+        const auto& key_path = rql_query.at(U("args")).at(0).as_string();
+        BST_REQUIRE_STRING_EQUAL(U("foo.bar.baz.qux"), key_path);
     }
 
     {
-        const utility::string_t query_rql = U("eq((foo,bar,baz%252Equx),quuux)");
+        const utility::string_t query_rql = U("eq((foo,bar,baz.qux),purr)");
         const auto rql_query = rql::parse_query(query_rql);
-        const auto key_path = get_key_path(rql_query.at(U("args")).at(0).as_array());
-
-        web::json::value results;
-        BST_REQUIRE_EQUAL(true, web::json::extract(test_value.as_object(), results, key_path));
-        BST_REQUIRE_EQUAL(U("quuux"), results.as_string());
+        const auto& key_path = rql_query.at(U("args")).at(0).as_array();
+        BST_REQUIRE_STRING_EQUAL(U("baz.qux"), key_path.rbegin()->as_string());
     }
 
     {
-        const utility::string_t query_rql = U("eq((foo,bar,foo),quuuux)");
+        const utility::string_t query_rql = U("eq((foo,bar,baz%2Equx),purr)");
         const auto rql_query = rql::parse_query(query_rql);
-        const auto key_path = get_key_path(rql_query.at(U("args")).at(0).as_array());
+        const auto& key_path = rql_query.at(U("args")).at(0).as_array();
+        BST_REQUIRE_STRING_EQUAL(U("baz.qux"), key_path.rbegin()->as_string());
+    }
 
-        web::json::value results;
-        BST_REQUIRE_EQUAL(true, web::json::extract(test_value.as_object(), results, key_path));
-        BST_REQUIRE_EQUAL(U("quuuux"), results.as_string());
+    {
+        const utility::string_t query_rql = U("eq((foo,bar,baz%252Equx),hiss)");
+        const auto rql_query = rql::parse_query(query_rql);
+        const auto& key_path = rql_query.at(U("args")).at(0).as_array();
+        BST_REQUIRE_STRING_EQUAL(U("baz%2Equx"), key_path.rbegin()->as_string());
     }
 }
