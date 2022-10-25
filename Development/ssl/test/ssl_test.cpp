c8e18f1c32f414ac733b203c0ef2a0c257688da7
lo-simon <simon.lo@sony.com>
2022-10-25 16:47:31 +0100

    Add some unit tests, more to come

--- Development/ssl/test/ssl_test.cpp
+++ Development/ssl/test/ssl_test.cpp
@@ -7,33 +7,96 @@
 ////////////////////////////////////////////////////////////////////////////////////////////
 BST_TEST_CASE(testSslParseErrors)
 {
-    ssl::experimental::X509_NAME_ptr x509_name(X509_NAME_new(), &X509_NAME_free);
-
-    struct test
+    struct x509_name_add_entry_by_txt_param
     {
         std::string field;
         std::string value;
+        int loc;
+        int set;
     };
 
-    const test tests[] =
+    // test against RFC 2253 examples
+    // see https://www.rfc-editor.org/rfc/rfc2253#section-5
     {
-        { "C", "UK"},
-        { "O", "Disorganized Organization"},
-        { "CN", "Joe Bloggs"}
-    };
-
-    // set up X509_NAME for the test
-    for (const auto& test : tests)
-    {
-        X509_NAME_add_entry_by_txt(x509_name.get(), test.field.c_str(), MBSTRING_ASC, (const unsigned char*)test.value.c_str(), -1, -1, 0);
+        // three relative distinguished names(RDNs): CN=Steve Kille,O=Isode Limited,C=GB
+        const x509_name_add_entry_by_txt_param x509_name_add_entry_by_txt_params[] =
+        {
+            { "CN", "Steve Kille", -1, 0 },
+            { "O", "Isode Limited", -1, 0 },
+            { "C", "GB", -1, 0 }
+        };
+        // set up X509 name to `CN=Steve Kille,O=Isode Limited,C=GB`
+        ssl::experimental::X509_NAME_ptr x509_name(X509_NAME_new(), &X509_NAME_free);
+        for (const auto& param : x509_name_add_entry_by_txt_params)
+        {
+            X509_NAME_add_entry_by_txt(x509_name.get(), param.field.c_str(), MBSTRING_ASC, (const unsigned char*)param.value.c_str(), -1, param.loc, param.set);
+        }
+        // run tests
+        for (const auto& param : x509_name_add_entry_by_txt_params)
+        {
+            BST_REQUIRE_EQUAL(param.value, ssl::experimental::details::get_attribute_value(x509_name.get(), param.field));
+        }
     }
 
-    // do test
-    for (const auto& test : tests)
     {
-        BST_REQUIRE_EQUAL(test.value, ssl::experimental::details::get_attribute_value(x509_name.get(), test.field));
+        // three RDNs, in which the first RDN is multi-valued: OU=Sales+CN=J.Smith,O=Widget Inc.,C=US
+        const x509_name_add_entry_by_txt_param x509_name_add_entry_by_txt_params[] =
+        {
+            { "OU", "Sales", -1, 0 },
+            { "CN", "J.Smith", 0, 1 },
+            { "O", "Widget Inc.", -1, 0 },
+            { "C", "US", -1, 0 }
+        };
+        // set up X509 name to `OU=Sales+CN=J.Smith,O=Widget Inc.,C=US`
+        ssl::experimental::X509_NAME_ptr x509_name(X509_NAME_new(), &X509_NAME_free);
+        for (const auto& param : x509_name_add_entry_by_txt_params)
+        {
+            X509_NAME_add_entry_by_txt(x509_name.get(), param.field.c_str(), MBSTRING_ASC, (const unsigned char*)param.value.c_str(), -1, param.loc, param.set);
+        }
+        // run tests
+        for (const auto& param : x509_name_add_entry_by_txt_params)
+        {
+            BST_REQUIRE_EQUAL(param.value, ssl::experimental::details::get_attribute_value(x509_name.get(), param.field));
+        }
     }
-    BST_REQUIRE_EQUAL("", ssl::experimental::details::get_attribute_value(x509_name.get(), " CN"));
-    BST_REQUIRE_EQUAL("", ssl::experimental::details::get_attribute_value(x509_name.get(), "CN "));
-    BST_REQUIRE_EQUAL("", ssl::experimental::details::get_attribute_value(x509_name.get(), "cn"));
+
+    {
+        // three RDNs, in which the second RDN has comma: CN=L. Eagle,O=Sue\, Grabbit and Runn,C=GB
+        const x509_name_add_entry_by_txt_param x509_name_add_entry_by_txt_params[] =
+        {
+            { "CN", "L. Eagle", -1, 0 },
+            { "O", "Sue, Grabbit and Runn", -1, 0 },
+            { "C", "GB", -1, 0 }
+        };
+        // set up X509 name to `CN=L. Eagle,O=Sue\, Grabbit and Runn,C=GB`
+        ssl::experimental::X509_NAME_ptr x509_name(X509_NAME_new(), &X509_NAME_free);
+        for (const auto& param : x509_name_add_entry_by_txt_params)
+        {
+            X509_NAME_add_entry_by_txt(x509_name.get(), param.field.c_str(), MBSTRING_ASC, (const unsigned char*)param.value.c_str(), -1, param.loc, param.set);
+        }
+        // run tests
+        for (const auto& param : x509_name_add_entry_by_txt_params)
+        {
+            BST_REQUIRE_EQUAL(param.value, ssl::experimental::details::get_attribute_value(x509_name.get(), param.field));
+        }
+    }
+
+    {
+        // two RDNs, missing CN RDN: OU=Sales,O=Widget Inc.,C=US
+        const x509_name_add_entry_by_txt_param x509_name_add_entry_by_txt_params[] =
+        {
+            { "OU", "Sales", -1, 0 },
+            { "O", "Widget Inc.", -1, 0 },
+            { "C", "US", -1, 0 }
+        };
+
+        // set up X509 name to `OU=Sales,O=Widget Inc.,C=US`
+        ssl::experimental::X509_NAME_ptr x509_name(X509_NAME_new(), &X509_NAME_free);
+        for (const auto& param : x509_name_add_entry_by_txt_params)
+        {
+            X509_NAME_add_entry_by_txt(x509_name.get(), param.field.c_str(), MBSTRING_ASC, (const unsigned char*)param.value.c_str(), -1, param.loc, param.set);
+        }
+        // do tests
+        BST_REQUIRE_EQUAL("", ssl::experimental::details::get_attribute_value(x509_name.get(), "CN"));
+    }
 }
