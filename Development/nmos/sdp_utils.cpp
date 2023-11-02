4f1844dbdfc9ea0df26fa6f728dcc10d05dffeae
Gareth Sylvester-Bradley <31761158+garethsb@users.noreply.github.com>
2023-11-02 07:08:50 -0400

    Fix SMPTE 291 exactframerate field (#346)
    * Add unit tests for SDP parameters creation/parsing functions for video/raw, audio/L, video/smpte291 and video/SMPTE2022-6
    
    * Fix insertion of exactframerate for video/smpte291
    (Thanks to @cmaberto, Cameron Maberto, Apple, for the original bug report.)
    
    * Fix bug generating VPID_Code on Linux due to char_t vs. uint8_t confusion
    
    * Fix SSN for ST2110-40:2023 to be ST2110-40:2021
    
    Although this revision should have been published in 2022 and the SSN should have been updated to match, neither of those things actually happened...
    
    Co-authored-by: jonathan-r-thorpe <64410119+jonathan-r-thorpe@users.noreply.github.com>
    
    * Update Development/nmos/test/sdp_utils_test.cpp
    
    ---------
    
    Co-authored-by: jonathan-r-thorpe <64410119+jonathan-r-thorpe@users.noreply.github.com>

--- Development/nmos/sdp_utils.cpp
+++ Development/nmos/sdp_utils.cpp
@@ -832,8 +832,8 @@ namespace nmos
         {
             return sdp_parameters::fmtp_t::value_type{ sdp::fields::DID_SDID, make_fmtp_did_sdid(did_sdid) };
         }));
-        if (0 != params.vpid_code) fmtp.push_back({ sdp::fields::VPID_Code, utility::ostringstreamed(params.vpid_code) });
-        if (!params.exactframerate) fmtp.push_back({ sdp::fields::exactframerate, nmos::details::make_exactframerate(params.exactframerate) });
+        if (0 != params.vpid_code) fmtp.push_back({ sdp::fields::VPID_Code, utility::ostringstreamed((uint32_t)params.vpid_code) });
+        if (0 != params.exactframerate) fmtp.push_back({ sdp::fields::exactframerate, nmos::details::make_exactframerate(params.exactframerate) });
         if (!params.tm.empty()) fmtp.push_back({ sdp::fields::TM, params.tm.name });
         if (!params.ssn.empty()) fmtp.push_back({ sdp::fields::smpte_standard_number, params.ssn.name });
         if (0 != params.troff) fmtp.push_back({ sdp::fields::TROFF, utility::ostringstreamed(params.troff) });
