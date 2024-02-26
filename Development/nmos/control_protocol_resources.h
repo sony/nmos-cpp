9406d81db579e3644df01c0399976365b784b24f
Jonathan Thorpe (Sony) <Jonathan.Thorpe@sony.com>
2024-02-26 16:46:20 +0000

    Added Receiver Monitor Protected and Ident Beacon workers to control_protocol_resources

--- Development/nmos/control_protocol_resources.h
+++ Development/nmos/control_protocol_resources.h
@@ -25,12 +25,30 @@ namespace nmos
     // create Class manager resource
     control_protocol_resource make_class_manager(nc_oid oid, const nmos::experimental::control_protocol_state& control_protocol_state);
 
+    // Monitoring feature set control classes
+    // 
     // create Receiver Monitor resource
     control_protocol_resource make_receiver_monitor(nc_oid oid, nmos::nc_oid owner, const utility::string_t& role, const utility::string_t& user_label, const utility::string_t& description, const web::json::value& touchpoints = web::json::value::null(), const web::json::value& runtime_property_constraints = web::json::value::null(),
         nc_connection_status::status connection_status = nc_connection_status::status::undefined,
         const utility::string_t& connection_status_message = U(""),
         nc_payload_status::status payload_status = nc_payload_status::status::undefined,
         const utility::string_t& payload_status_message = U("")
+    );
+
+    // create Receiver Monitor Protected resource
+    control_protocol_resource make_receiver_monitor_protected(nc_oid oid, nmos::nc_oid owner, const utility::string_t& role, const utility::string_t& user_label, const utility::string_t& description, const web::json::value& touchpoints, const web::json::value& runtime_property_constraints = web::json::value::null(),
+        nc_connection_status::status connection_status = nc_connection_status::status::undefined,
+        const utility::string_t& connection_status_message = U(""),
+        nc_payload_status::status payload_status = nc_payload_status::status::undefined,
+        const utility::string_t& payload_status_message = U(""),
+        bool signal_protection_status = true
+    );
+
+    // Identification feature set control classes
+    // 
+    // create Ident Beacon resource
+    control_protocol_resource make_ident_beacon(nc_oid oid, nmos::nc_oid owner, const utility::string_t& role, const utility::string_t& user_label, const utility::string_t& description, const web::json::value& touchpoints = web::json::value::null(), const web::json::value& runtime_property_constraints = web::json::value::null(),
+        bool active = false
     );
 }
 
