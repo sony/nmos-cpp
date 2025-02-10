90b956fcfb11ddbde9e9a3fba13cc4e84f7cfc9b
Jonathan Thorpe (Sony) <Jonathan.Thorpe@sony.com>
2025-02-10 17:00:50 +0000

    Formatting

--- Development/cmake/NmosCppTest.cmake
+++ Development/cmake/NmosCppTest.cmake
@@ -44,7 +44,7 @@ set(NMOS_CPP_TEST_NMOS_TEST_SOURCES
     nmos/test/capabilities_test.cpp
     nmos/test/channels_test.cpp
     nmos/test/control_protocol_test.cpp
-	nmos/test/control_protocol_methods_test.cpp
+    nmos/test/control_protocol_methods_test.cpp
     nmos/test/did_sdid_test.cpp
     nmos/test/event_type_test.cpp
     nmos/test/json_validator_test.cpp
