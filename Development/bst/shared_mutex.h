e1e44ec6fbb6178ad1fb0b4da3dbffb53b419293
lo-simon <simon.lo@sony.com>
2025-02-21 20:30:39 +0000

    typo

--- Development/bst/shared_mutex.h
+++ Development/bst/shared_mutex.h
@@ -17,7 +17,7 @@ namespace bst_shared_mutex_condition_variable_any = std;
 namespace bst_shared_mutex = boost;
 
 #ifdef _WIN32
-// Note:: Windows boost::condition_variable_any::wait would throw nested lock expectations when boost::shared_mutex has reached the
+// Note:: Windows boost::condition_variable_any::wait would throw nested lock exceptions when boost::shared_mutex has reached the
 // maximum number of exclusive_waiting locks.A modified version of the boost::condition_variable_any was created to address this issue.
 #include "boost/thread/win32_condition_variable.hpp"
 namespace bst_shared_mutex_condition_variable_any = boost::experimental;
